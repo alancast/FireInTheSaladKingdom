ef7416bc0bf23bfb4d7b24beef91ae3552de826e
James McNulty <jimmcnulty41@gmail.com>
2015-01-18 17:13:30 -0500

    Added tiles

--- Assets/LevelElements/Triggers/CoinTrigger.cs
+++ Assets/LevelElements/Triggers/CoinTrigger.cs
@@ -6,6 +6,7 @@ public class CoinTrigger : MonoBehaviour {
 	void OnTriggerEnter(){
 		Destroy (this.gameObject);
 		CameraMGR.instance.score += 1;
-		CameraMGR.instance.scoreGT.text = "Coins: " + CameraMGR.instance.score;
+		CameraMGR.instance.scoreGT.text = "Coins: " + CameraMGR.instance.score + "/" 
+			+ (FireTrigger.instance.count - 5);
 	}
 }
--- Assets/LevelElements/Triggers/FireTrigger.cs
+++ Assets/LevelElements/Triggers/FireTrigger.cs
@@ -2,14 +2,22 @@
 using System.Collections;
 
 public class FireTrigger : MonoBehaviour {
-	private int count;
+	public static FireTrigger instance;
+	public int count;
 
 	void Awake(){
 		count = GetComponentsInChildren<Transform> ().Length - 1;
+		instance = this;
+		
+	}
+
+	void Start(){
+		CameraMGR.instance.scoreGT.text = "Coins: " +
+			CameraMGR.instance.score + "/" + (count - 5);
 	}
 
 	void OnTriggerEnter(Collider other){
-		if (CameraMGR.instance.score == count) {
+		if (CameraMGR.instance.score >= count - 5) {
 			print ("win");			
 		}
 	}
