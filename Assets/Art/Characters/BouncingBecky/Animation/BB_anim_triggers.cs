b86648ee719bccc885e03902c6d162ca53d084b4
James McNulty <jimmcnulty41@gmail.com>
2015-06-19 21:09:04 -0400

    Added artwork

---
+++ Assets/Art/Characters/BouncingBecky/Animation/BB_anim_triggers.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BB_anim_triggers : MonoBehaviour {
+
+	Animator anim;
+	Rigidbody rigid;
+
+	public float whistle_speed = .2f;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		rigid = GetComponentInParent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rigid.velocity.magnitude > whistle_speed) anim.SetBool("Moving", true);
+		else anim.SetBool("Moving", false);
+	}
+}
---
+++ Assets/Art/Characters/SpeedySachin/Animation/SS_anim_triggers.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SS_anim_triggers : MonoBehaviour {
+	
+	Animator anim;
+	Rigidbody rigid;
+	
+	public float sneeze_speed = .2f;
+	
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		rigid = GetComponentInParent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rigid.velocity.magnitude > sneeze_speed) anim.SetBool("Moving", true);
+		else anim.SetBool("Moving", false);
+	}
+}
